e22e61c1a6c3ab28ec654d6ac6fc53fa08b7329a
Patryk <patryk.wierzbicki16@gmail.com>
2026-07-02 17:40:46 +0200

    fix port

--- serwer.js
+++ serwer.js
@@ -5,7 +5,7 @@ import kalk from './kalkulator.js';
 import db from './baza.js';
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
